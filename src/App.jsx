2d3ea67badd07fa12b917ce83c6c9f1f72ff28f7
Andy Pugh <63121748+imandypugh@users.noreply.github.com>
2025-11-30 21:33:57 +0000

    udaptes

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ function App() {
           
           <div className="detail-item location-item">
             <div className="detail-icon-wrapper">
-              <div className="detail-icon">☕</div>
+              <div className="detail-icon">🥂</div>
             </div>
             <div className="detail-content">
               <h3>Location</h3>
@@ -300,11 +300,21 @@ function App() {
         <p className="closing">We can't wait to celebrate with you!</p>
         
         <div className="footer-hearts">
-          <span>💕</span>
-          <span>💖</span>
-          <span>💝</span>
-          <span>💗</span>
-          <span>💕</span>
+          <svg className="heart-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="white" stroke="#D4AF37" strokeWidth="1.5"/>
+          </svg>
+          <svg className="heart-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="white" stroke="#D4AF37" strokeWidth="1.5"/>
+          </svg>
+          <svg className="heart-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="white" stroke="#D4AF37" strokeWidth="1.5"/>
+          </svg>
+          <svg className="heart-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="white" stroke="#D4AF37" strokeWidth="1.5"/>
+          </svg>
+          <svg className="heart-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="white" stroke="#D4AF37" strokeWidth="1.5"/>
+          </svg>
         </div>
       </div>
     </div>
